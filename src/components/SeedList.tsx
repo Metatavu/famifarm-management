ac1a0d30aaa2845b4b1da33f900e06a10f4cf807
Antti Leppä <antti.leppa@metatavu.fi>
2019-03-21 16:35:16 +0200

    Fixed error handling on SeedList

--- src/components/SeedList.tsx
+++ src/components/SeedList.tsx
@@ -1,9 +1,13 @@
 import * as React from "react";
 import * as Keycloak from 'keycloak-js';
+import * as actions from "../actions";
 import Api from "../api";
 import { NavLink } from 'react-router-dom';
 import { Seed } from "famifarm-typescript-models";
 import strings from "src/localization/strings";
+import { StoreState, ErrorMessage } from "../types/index";
+import { connect } from "react-redux";
+import { Dispatch } from "redux";
 
 import {
   List,
@@ -16,7 +20,8 @@ import LocalizedUtils from "src/localization/localizedutils";
 export interface Props {
   keycloak?: Keycloak.KeycloakInstance;
   seeds?: Seed[];
-  onSeedsFound?: (seeds: Seed[]) => void;
+  onSeedsFound?: (seeds: Seed[]) => void,
+  onError: (error: ErrorMessage) => void
 }
 
 export interface State {
@@ -34,25 +39,32 @@ class SeedsList extends React.Component<Props, State> {
   /**
    * Component did mount life-sycle event
    */
-  async componentDidMount() {
-    if (!this.props.keycloak) {
-      return;
-    }
+  public async componentDidMount() {
+    try {
+      if (!this.props.keycloak) {
+        return;
+      }
 
-    const seedsService = await Api.getSeedsService(this.props.keycloak);
-    seedsService.listSeeds().then((seeds) => {
+      const seedsService = await Api.getSeedsService(this.props.keycloak);
+      const seeds = await seedsService.listSeeds();
       this.props.onSeedsFound && this.props.onSeedsFound(seeds);
-    });
+    } catch (e) {
+      this.props.onError({
+        message: strings.defaultApiErrorMessage,
+        title: strings.defaultApiErrorTitle,
+        exception: e
+      });
+    }
   }
 
   /**
    * Render seed list view
    */
-  render() {
+  public render() {
     if (!this.props.seeds) {
       return (
         <Grid style={{paddingTop: "100px"}} centered>
-          <Loader active size="medium" />
+          <Loader inline active size="medium" />
         </Grid>
       );
     }
@@ -91,4 +103,28 @@ class SeedsList extends React.Component<Props, State> {
   }
 }
 
-export default SeedsList;
+/**
+ * Redux mapper for mapping store state to component props
+ * 
+ * @param state store state
+ */
+export function mapStateToProps(state: StoreState) {
+  return {
+    seeds: state.seeds,
+    seed: state.seed
+  };
+}
+
+/**
+ * Redux mapper for mapping component dispatches 
+ * 
+ * @param dispatch dispatch method
+ */
+export function mapDispatchToProps(dispatch: Dispatch<actions.AppAction>) {
+  return {
+    onSeedsFound: (seeds: Seed[]) => dispatch(actions.seedsFound(seeds)),
+    onError: (error: ErrorMessage) => dispatch(actions.onErrorOccurred(error)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SeedsList);
